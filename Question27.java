94a41869f21ac0d31596cb30523d89b335196169
Anarghya Kinila <88137286+anarghyakinila88@users.noreply.github.com>
2021-07-31 20:47:34 +0530

    Add files via upload

---
+++ Question27.java
@@ -0,0 +1,14 @@
+package HundredQuestions;
+
+public class Question27 {
+    public static void main(String[] args) {
+        int base = 12;
+        int power = 5;
+        int result = 1;
+        while (power != 0) {
+            result = result * base;
+            --power;
+        }
+        System.out.println("Result : " + result);
+    }
+}
