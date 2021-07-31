7bb8160609f2871e09bf5b2c1140670eb4afcb29
Anarghya Kinila <88137286+anarghyakinila88@users.noreply.github.com>
2021-07-31 20:25:30 +0530

    Add files via upload

---
+++ Question12.java
@@ -0,0 +1,11 @@
+package HundredQuestions;
+
+public class Question12 {
+    public static void main(String[] args) {
+        char ch = 'a';
+        int ascii = ch;
+        int cast = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + cast);
+    }
+}
